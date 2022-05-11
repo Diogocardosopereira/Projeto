8de15e31a8b24c7a0f756e70fba1920c95e42ade
diogo caardoso pereira <diogo.pereira9@aluno.ce.gov.br>
2022-05-11 10:51:06 -0300

    ja

---
+++ Login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Login </title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(45deg,black,  );
+        }
+        div{
+            background-color: rgba(0, 0, 0, 0.8);
+            position: absolute;
+            top: 38%;
+            left: 38%;
+            transform: translate(-45,-45%);
+            padding: 45px;
+            border-radius: 30px;
+            background-color: red;
+            border-bottom-color: red;
+            color: white #fff
+
+        }
+        input{
+            padding: 15px;
+            border: none;
+            outline: none;
+            font-size: 15px;
+        }
+       button{
+           background-color: dodgerblue;
+           border: none;
+           padding: 15px;
+           width: 100%;
+           border-radius: 10px;
+           color: white;
+           font-size: 15px; 
+
+       }
+       button:hover{
+           background-color: dodgerblue;
+           cursor: pointer;
+
+        }
+        </style> 
+    
+    </head>
+<body> 
+    <div>
+    <h1>Login</h1>
+        <br><br>
+        <input placeholder="Email" type="Email"> 
+        <br><br>
+        <input placeholder="senha" type="Text"> 
+        <br><br>
+        <input type="button" value="Entrar"> 
+       
+        </html>
---
+++ cadrasto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academia</title>
+</head>
+<body>
+    <center>
+      <h1>Cadrasto de usuario</h1>
+    <form>  
+       <label for="name">nome completo</label>
+       <input type="text" class="form-control" name="nome">
+
+       <label for="email">email</label>
+       <input type="email" class="form-control" name="email">
+
+       <label for="endereço">endereço</label>
+       <input type="text" class="form-control" name="endereço">
+       <input type="submit">
+   
+       </center>
+    </form>    
+</body>
+</html>
